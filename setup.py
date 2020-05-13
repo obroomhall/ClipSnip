891d71ad322b24df2a92328febbe78b55e80fe64
Oliver Broomhall <obroomhall@gmail.com>
2020-05-14 00:18:50 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='autotrim',
-    version='0.0.7',
+    version='0.0.8',
     packages=setuptools.find_packages(),
     url='https://github.com/obroomhall/autotrim',
     author='Oliver Broomhall',
